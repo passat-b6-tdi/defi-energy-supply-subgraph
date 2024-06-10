3621de214c4b1d8e5f4c65291ba48cc93384f6e3
big B <57547705+b0gdaniy@users.noreply.github.com>
2024-06-10 14:10:15 +0300

    fix: mappings

--- src/mappings/energy-oracle.ts
+++ src/mappings/energy-oracle.ts
@@ -1,28 +1,15 @@
 import {
   EnergyConsumptionRecorded as EnergyConsumptionRecordedEvent,
   EnergyConsumptionSent as EnergyConsumptionSentEvent,
-  OutlierDetected as OutlierDetectedEvent,
-  Paused as PausedEvent,
-  RoleAdminChanged as RoleAdminChangedEvent,
-  RoleGranted as RoleGrantedEvent,
-  RoleRevoked as RoleRevokedEvent,
-  Unpaused as UnpausedEvent,
-} from "../../generated/EnergyOracle/EnergyOracle"
+} from "../types/EnergyOracle/EnergyOracle"
 import {
-  EnergyConsumptionRecorded,
-  EnergyConsumptionSent,
-  OutlierDetected,
-  Paused,
-  RoleAdminChanged,
-  RoleGranted,
-  RoleRevoked,
-  Unpaused,
-} from "../../generated/schema"
+  EnergyConsumption,
+} from "../types/schema"
 
 export function handleEnergyConsumptionRecorded(
   event: EnergyConsumptionRecordedEvent,
 ): void {
-  let entity = new EnergyConsumptionRecorded(
+  let entity = new EnergyConsumption(
     event.transaction.hash.concatI32(event.logIndex.toI32()),
   )
   entity.sender = event.params.sender
@@ -41,7 +28,7 @@ export function handleEnergyConsumptionRecorded(
 export function handleEnergyConsumptionSent(
   event: EnergyConsumptionSentEvent,
 ): void {
-  let entity = new EnergyConsumptionSent(
+  let entity = new EnergyConsumption(
     event.transaction.hash.concatI32(event.logIndex.toI32()),
   )
   entity.sender = event.params.sender
@@ -49,94 +36,6 @@ export function handleEnergyConsumptionSent(
   entity.supplierId = event.params.supplierId
   entity.timestamp = event.params.timestamp
   entity.consumption = event.params.consumption
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleOutlierDetected(event: OutlierDetectedEvent): void {
-  let entity = new OutlierDetected(
-    event.transaction.hash.concatI32(event.logIndex.toI32()),
-  )
-  entity.sender = event.params.sender
-  entity.user = event.params.user
-  entity.supplierId = event.params.supplierId
-  entity.timestamp = event.params.timestamp
-  entity.consumption = event.params.consumption
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handlePaused(event: PausedEvent): void {
-  let entity = new Paused(
-    event.transaction.hash.concatI32(event.logIndex.toI32()),
-  )
-  entity.account = event.params.account
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleRoleAdminChanged(event: RoleAdminChangedEvent): void {
-  let entity = new RoleAdminChanged(
-    event.transaction.hash.concatI32(event.logIndex.toI32()),
-  )
-  entity.role = event.params.role
-  entity.previousAdminRole = event.params.previousAdminRole
-  entity.newAdminRole = event.params.newAdminRole
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleRoleGranted(event: RoleGrantedEvent): void {
-  let entity = new RoleGranted(
-    event.transaction.hash.concatI32(event.logIndex.toI32()),
-  )
-  entity.role = event.params.role
-  entity.account = event.params.account
-  entity.sender = event.params.sender
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleRoleRevoked(event: RoleRevokedEvent): void {
-  let entity = new RoleRevoked(
-    event.transaction.hash.concatI32(event.logIndex.toI32()),
-  )
-  entity.role = event.params.role
-  entity.account = event.params.account
-  entity.sender = event.params.sender
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleUnpaused(event: UnpausedEvent): void {
-  let entity = new Unpaused(
-    event.transaction.hash.concatI32(event.logIndex.toI32()),
-  )
-  entity.account = event.params.account
 
   entity.blockNumber = event.block.number
   entity.blockTimestamp = event.block.timestamp
--- src/mappings/register.ts
+++ src/mappings/register.ts
@@ -1,69 +1,17 @@
 import {
-  RoleAdminChanged as RoleAdminChangedEvent,
-  RoleGranted as RoleGrantedEvent,
-  RoleRevoked as RoleRevokedEvent,
   SupplierRegistered as SupplierRegisteredEvent,
   SupplierUnregistered as SupplierUnregisteredEvent,
   UserRegistered as UserRegisteredEvent,
   UserUnregistered as UserUnregisteredEvent
-} from "../../generated/Register/Register"
+} from "../types/Register/Register"
 import {
-  RoleAdminChanged,
-  RoleGranted,
-  RoleRevoked,
-  SupplierRegistered,
-  SupplierUnregistered,
-  UserRegistered,
-  UserUnregistered
-} from "../../generated/schema"
+  Supplier,
+  User
+} from "../types/schema"
 
-export function handleRoleAdminChanged(event: RoleAdminChangedEvent): void {
-  let entity = new RoleAdminChanged(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.role = event.params.role
-  entity.previousAdminRole = event.params.previousAdminRole
-  entity.newAdminRole = event.params.newAdminRole
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleRoleGranted(event: RoleGrantedEvent): void {
-  let entity = new RoleGranted(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.role = event.params.role
-  entity.account = event.params.account
-  entity.sender = event.params.sender
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleRoleRevoked(event: RoleRevokedEvent): void {
-  let entity = new RoleRevoked(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.role = event.params.role
-  entity.account = event.params.account
-  entity.sender = event.params.sender
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
 
 export function handleSupplierRegistered(event: SupplierRegisteredEvent): void {
-  let entity = new SupplierRegistered(
+  let entity = new Supplier(
     event.transaction.hash.concatI32(event.logIndex.toI32())
   )
   entity.sender = event.params.sender
@@ -80,7 +28,7 @@ export function handleSupplierRegistered(event: SupplierRegisteredEvent): void {
 export function handleSupplierUnregistered(
   event: SupplierUnregisteredEvent
 ): void {
-  let entity = new SupplierUnregistered(
+  let entity = new Supplier(
     event.transaction.hash.concatI32(event.logIndex.toI32())
   )
   entity.sender = event.params.sender
@@ -95,7 +43,7 @@ export function handleSupplierUnregistered(
 }
 
 export function handleUserRegistered(event: UserRegisteredEvent): void {
-  let entity = new UserRegistered(
+  let entity = new User(
     event.transaction.hash.concatI32(event.logIndex.toI32())
   )
   entity.sender = event.params.sender
@@ -110,7 +58,7 @@ export function handleUserRegistered(event: UserRegisteredEvent): void {
 }
 
 export function handleUserUnregistered(event: UserUnregisteredEvent): void {
-  let entity = new UserUnregistered(
+  let entity = new User(
     event.transaction.hash.concatI32(event.logIndex.toI32())
   )
   entity.sender = event.params.sender
