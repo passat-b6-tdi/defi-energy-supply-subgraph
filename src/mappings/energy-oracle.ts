110bcbdcbaa9934f7b23f91b7a50065e5dc67064
Bohdan Pukhno <pukhnobg@gmail.com>
2024-06-20 14:14:18 +0300

    upd: mappings

--- src/mappings/energy-oracle.ts
+++ src/mappings/energy-oracle.ts
@@ -1,26 +1,24 @@
 import {
   EnergyConsumptionRecorded as EnergyConsumptionRecordedEvent,
   EnergyConsumptionPaid as EnergyConsumptionPaidEvent,
+  EnergyProductionRecorded as EnergyProductionRecordedEvent,
 } from "../types/EnergyOracle/EnergyOracle"
 import {
-  Consumer
-} from "../wrappers/consumer"
-import {
   EnergyConsumption
-} from "../wrappers/energy-oracle"
+} from "../wrappers/energy-consumption"
+import {
+  EnergyProduction
+} from "../wrappers/energy-production"
 import { BigInt } from "@graphprotocol/graph-ts"
-import { Supplier } from "../wrappers/supplier"
 
 export function handleEnergyConsumptionRecorded(
   event: EnergyConsumptionRecordedEvent,
 ): void {
   const consumerAddress = event.params.whoseConsumption.toHex()
   const supplierId = event.params.supplierId.toHex()
-
   const consumerId = consumerAddress.concat('-').concat(supplierId)
 
   let consumption = EnergyConsumption.mustLoad(consumerId)
-
   consumption.lastUpdateTimestamp = event.params.timestamp
   consumption.consumption = event.params.consumption
 
@@ -32,13 +30,25 @@ export function handleEnergyConsumptionSent(
 ): void {
   const consumerAddress = event.params.whoseConsumption.toHex()
   const supplierId = event.params.supplierId.toHex()
+  const consumerId = consumerAddress.concat('-').concat(supplierId)
 
-  const userId = consumerAddress.concat('-').concat(supplierId)
-
-  let consumption = EnergyConsumption.mustLoad(userId)
-
+  let consumption = EnergyConsumption.mustLoad(consumerId)
   consumption.lastUpdateTimestamp = event.params.timestamp
   consumption.consumption = BigInt.fromI32(0)
 
   consumption.save()
+}
+
+export function handleEnergyProductionRecorded(
+  event: EnergyProductionRecordedEvent,
+): void {
+  const supplierAddress = event.params.supplier.toHex()
+  const supplierId = event.params.supplierId.toHex()
+  const id = supplierAddress.concat('-').concat(supplierId)
+
+  let production = EnergyProduction.mustLoad(id)
+  production.lastUpdateTimestamp = event.params.timestamp
+  production.production = event.params.production
+
+  production.save()
 }
--- src/mappings/register.ts
+++ src/mappings/register.ts
@@ -11,21 +11,26 @@ import {
 import {
   Consumer
 } from "../wrappers/consumer"
-import { EnergyConsumption } from "../wrappers/energy-oracle"
+import { EnergyConsumption } from "../wrappers/energy-consumption"
+import { EnergyProduction } from "../wrappers/energy-production"
 
 
 export function handleSupplierRegistered(event: SupplierRegisteredEvent): void {
   const supplierAddress = event.params.supplier
   const supplierId = event.params.supplierId
-
   const supplierAddressAndId = supplierAddress.toHex().concat('-').concat(supplierId.toHex())
 
   let supplier = Supplier.loadOrCreate(supplierAddressAndId)
-
   supplier.supplierId = supplierId
   supplier.supplierAddress = supplierAddress
 
+  let production = EnergyProduction.loadOrCreate(supplier.id)
+  production.supplier = supplierAddress.toHex()
+  production.lastUpdateTimestamp = event.block.timestamp
+  production.production = BigInt.fromI32(0)
+
   supplier.save()
+  production.save()
 }
 
 export function handleSupplierUnregistered(
@@ -33,17 +38,22 @@ export function handleSupplierUnregistered(
 ): void {
   const supplierAddress = event.params.supplier
   const supplierId = event.params.supplierId
-
   const supplierAddressAndId = supplierAddress.toHex().concat('-').concat(supplierId.toHex())
 
   let supplier = Supplier.mustLoad(supplierAddressAndId)
-
   supplier.supplierId = BigInt.fromI32(0)
   supplier.supplierAddress = Bytes.fromHexString('')
 
+  let production = EnergyProduction.mustLoad(supplier.id)
+  production.supplier = ''
+  production.lastUpdateTimestamp = BigInt.fromI32(0)
+  production.production = BigInt.fromI32(0)
+
   supplier.save()
+  production.save()
 
   store.remove('Supplier', supplier.id)
+  store.remove('EnergyProduction', supplier.id)
 }
 
 export function handleUserRegistered(event: ConsumerRegisteredEvent): void {
@@ -51,13 +61,14 @@ export function handleUserRegistered(event: ConsumerRegisteredEvent): void {
   const supplierAddress = event.params.supplierAddress.toHex()
   const supplierAddressAndId = supplierAddress.concat('-').concat(supplierId)
 
-  const consumerAddress = event.params.consumer.toHex()
-  const consumerAddressAndSupplierId = consumerAddress.concat('-').concat(supplierId)
+  const consumerAddress = event.params.consumer
+  const consumerId = consumerAddress.toHex().concat('-').concat(supplierId)
 
-  let consumer = Consumer.loadOrCreate(consumerAddressAndSupplierId)
-  let consumption = EnergyConsumption.loadOrCreate(consumerAddressAndSupplierId)
-
+  let consumer = Consumer.loadOrCreate(consumerId)
   consumer.supplier = supplierAddressAndId
+  consumer.consumerAddress = consumerAddress
+
+  let consumption = EnergyConsumption.loadOrCreate(consumerId)
   consumption.consumer = consumer.id
   consumption.lastUpdateTimestamp = event.block.timestamp
   consumption.consumption = BigInt.fromI32(0)
@@ -72,10 +83,10 @@ export function handleUserUnregistered(event: ConsumerUnregisteredEvent): void {
   let consumerId = event.params.consumer.toHex().concat('-').concat(supplierId)
 
   let consumer = Consumer.mustLoad(consumerId)
-  let consumption = EnergyConsumption.mustLoad(consumerId)
-
   consumer.supplier = ''
+  consumer.consumerAddress = Bytes.fromHexString('')
 
+  let consumption = EnergyConsumption.mustLoad(consumerId)
   consumption.consumer = ''
   consumption.lastUpdateTimestamp = BigInt.fromI32(0)
   consumption.consumption = BigInt.fromI32(0)
