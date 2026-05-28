ad56ef9d7d2391a9bf743c6aa355078ba936f124
Bohdan Pukhno <pukhnobg@gmail.com>
2026-05-28 16:34:14 +0300

    feat: update mappings

--- src/mappings/register.ts
+++ src/mappings/register.ts
@@ -1,99 +1,87 @@
-import { BigInt, Bytes, store } from "@graphprotocol/graph-ts"
+import { BigInt, store } from "@graphprotocol/graph-ts";
 import {
+  ProducerRegistered as ProducerRegisteredEvent,
+  ProducerUnregistered as ProducerUnregisteredEvent,
   SupplierRegistered as SupplierRegisteredEvent,
   SupplierUnregistered as SupplierUnregisteredEvent,
   ConsumerRegistered as ConsumerRegisteredEvent,
-  ConsumerUnregistered as ConsumerUnregisteredEvent
-} from "../types/Register/Register"
-import {
-  Supplier
-} from "../wrappers/supplier"
-import {
-  Consumer
-} from "../wrappers/consumer"
-import { EnergyConsumption } from "../wrappers/energy-consumption"
-import { EnergyProduction } from "../wrappers/energy-production"
+  ConsumerUnregistered as ConsumerUnregisteredEvent,
+} from "../types/Register/Register";
+import { Producer } from "../wrappers/producer";
+import { Supplier } from "../wrappers/supplier";
+import { Consumer } from "../wrappers/consumer";
+import { EnergyConsumption } from "../wrappers/energy-consumption";
+import { EnergyProduction } from "../wrappers/energy-production";
 
+export function handleProducerRegistered(event: ProducerRegisteredEvent): void {
+  const producerId = event.params.producerId;
+  const id = producerId.toHex();
+
+  let producer = Producer.loadOrCreate(id);
+  producer.producerId = producerId;
+  producer.producerAddress = event.params.producer;
+
+  producer.save();
+}
+
+export function handleProducerUnregistered(
+  event: ProducerUnregisteredEvent,
+): void {
+  store.remove("Producer", event.params.producerId.toHex());
+}
 
 export function handleSupplierRegistered(event: SupplierRegisteredEvent): void {
-  const supplierAddress = event.params.supplier
-  const supplierId = event.params.supplierId
-  const supplierAddressAndId = supplierAddress.toHex().concat('-').concat(supplierId.toHex())
+  const supplierAddress = event.params.supplier;
+  const supplierId = event.params.supplierId;
+  const id = supplierId.toHex();
 
-  let supplier = Supplier.loadOrCreate(supplierAddressAndId)
-  supplier.supplierId = supplierId
-  supplier.supplierAddress = supplierAddress
+  let supplier = Supplier.loadOrCreate(id);
+  supplier.supplierId = supplierId;
+  supplier.supplierAddress = supplierAddress;
 
-  let production = EnergyProduction.loadOrCreate(supplier.id)
-  production.supplier = supplier.id
-  production.lastUpdateTimestamp = event.block.timestamp
-  production.production = BigInt.fromI32(0)
+  let production = EnergyProduction.loadOrCreate(id);
+  production.supplier = supplier.id;
+  production.lastUpdateTimestamp = event.block.timestamp;
+  production.production = BigInt.fromI32(0);
 
-  supplier.save()
-  production.save()
+  supplier.save();
+  production.save();
 }
 
 export function handleSupplierUnregistered(
-  event: SupplierUnregisteredEvent
+  event: SupplierUnregisteredEvent,
 ): void {
-  const supplierAddress = event.params.supplier
-  const supplierId = event.params.supplierId
-  const supplierAddressAndId = supplierAddress.toHex().concat('-').concat(supplierId.toHex())
+  const id = event.params.supplierId.toHex();
 
-  let supplier = Supplier.mustLoad(supplierAddressAndId)
-  supplier.supplierId = BigInt.fromI32(0)
-  supplier.supplierAddress = Bytes.fromHexString('')
-
-  let production = EnergyProduction.mustLoad(supplier.id)
-  production.supplier = ''
-  production.lastUpdateTimestamp = BigInt.fromI32(0)
-  production.production = BigInt.fromI32(0)
-
-  supplier.save()
-  production.save()
-
-  store.remove('Supplier', supplier.id)
-  store.remove('EnergyProduction', supplier.id)
+  store.remove("Supplier", id);
+  store.remove("EnergyProduction", id);
 }
 
 export function handleUserRegistered(event: ConsumerRegisteredEvent): void {
-  const supplierId = event.params.supplierId.toHex()
-  const supplierAddress = event.params.supplierAddress.toHex()
-  const supplierAddressAndId = supplierAddress.concat('-').concat(supplierId)
+  const supplierKey = event.params.supplierId.toHex();
+  const consumerAddress = event.params.consumer;
+  const consumerId = consumerAddress.toHex().concat("-").concat(supplierKey);
 
-  const consumerAddress = event.params.consumer
-  const consumerId = consumerAddress.toHex().concat('-').concat(supplierId)
+  let consumer = Consumer.loadOrCreate(consumerId);
+  consumer.supplier = supplierKey;
+  consumer.consumerAddress = consumerAddress;
 
-  let consumer = Consumer.loadOrCreate(consumerId)
-  consumer.supplier = supplierAddressAndId
-  consumer.consumerAddress = consumerAddress
+  let consumption = EnergyConsumption.loadOrCreate(consumerId);
+  consumption.consumer = consumer.id;
+  consumption.lastUpdateTimestamp = event.block.timestamp;
+  consumption.consumption = BigInt.fromI32(0);
 
-  let consumption = EnergyConsumption.loadOrCreate(consumerId)
-  consumption.consumer = consumer.id
-  consumption.lastUpdateTimestamp = event.block.timestamp
-  consumption.consumption = BigInt.fromI32(0)
-
-  consumer.save()
-  consumption.save()
+  consumer.save();
+  consumption.save();
 }
 
 export function handleUserUnregistered(event: ConsumerUnregisteredEvent): void {
-  const supplierId = event.params.supplierId.toHex()
+  const supplierKey = event.params.supplierId.toHex();
+  const consumerId = event.params.consumer
+    .toHex()
+    .concat("-")
+    .concat(supplierKey);
 
-  let consumerId = event.params.consumer.toHex().concat('-').concat(supplierId)
-
-  let consumer = Consumer.mustLoad(consumerId)
-  consumer.supplier = ''
-  consumer.consumerAddress = Bytes.fromHexString('')
-
-  let consumption = EnergyConsumption.mustLoad(consumerId)
-  consumption.consumer = ''
-  consumption.lastUpdateTimestamp = BigInt.fromI32(0)
-  consumption.consumption = BigInt.fromI32(0)
-
-  consumer.save()
-  consumption.save()
-
-  store.remove('Consumer', consumer.id)
-  store.remove('EnergyConsumption', consumption.id)
+  store.remove("Consumer", consumerId);
+  store.remove("EnergyConsumption", consumerId);
 }
